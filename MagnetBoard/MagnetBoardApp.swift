bd79177559d76cb16dc98bfd25dd067ef8291716
Erik Petersen <erik.petersen@gtl.net>
2026-05-25 10:22:46 -0700

    Initial Commit

---
+++ MagnetBoard/MagnetBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnetBoardApp.swift
+//  MagnetBoard
+//
+//  Created by Erik Petersen on 5/25/26.
+//
+
+import SwiftUI
+
+@main
+struct MagnetBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
